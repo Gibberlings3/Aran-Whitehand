3a528d0203d3babd5e89e99a54a7466033dd41bf
cmorganbg <cmorganbg@gmail.com>
2019-01-26 13:51:39 -0600

    Moving Timers A_R to SUBCOMPONENT
    Lots of changes to install behavior - pass #1. Moved A_R from project timers to SUBCOMPONENT. Rejiggered custom log. Moved most .tp2 to .tra references. Amellg - repair of typo I missed the first time. More.

---
+++ aranw/dialog/c-arantimers2hours.d
@@ -0,0 +1,11 @@
+
+/* Change the installed timers in .dlg to values that are a minimum of 2 hours real time before the next talk can trigger */
+/* all timers (after initial .bcs) in the project are set through dialog, and they all are the same length - the initial .bcs block staggers the start out to try to make it less likely that multiple events align */
+
+REPLACE_ACTION_TEXT C-ARANJ ~RealSetGlobalTimer("c-aranfriendtimer","GLOBAL",\([^)]+\))~ ~RealSetGlobalTimer("c-aranromtimer","GLOBAL",7205)~
+REPLACE_ACTION_TEXT C-ARANJ ~RealSetGlobalTimer("c-aranromtimer","GLOBAL",\([^)]+\))~ ~RealSetGlobalTimer("c-aranromtimer","GLOBAL",7205)~
+REPLACE_ACTION_TEXT C-ARANJ ~RealSetGlobalTimer("c-aranflirttimer","GLOBAL",\([^)]+\))~ ~RealSetGlobalTimer("c-aranromtimer","GLOBAL",7205)~
+REPLACE_ACTION_TEXT C-ARN25J ~RealSetGlobalTimer("c-aranfriendtimer","GLOBAL",\([^)]+\))~ ~RealSetGlobalTimer("c-aranromtimer","GLOBAL",7205)~
+REPLACE_ACTION_TEXT C-ARN25J ~RealSetGlobalTimer("c-aranromtimer","GLOBAL",\([^)]+\))~ ~RealSetGlobalTimer("c-aranromtimer","GLOBAL",7205)~
+REPLACE_ACTION_TEXT C-ARN25J ~RealSetGlobalTimer("c-aranromtimer","GLOBAL",\([^)]+\))~ ~RealSetGlobalTimer("c-aranflirttimer","GLOBAL",7205)~
+
